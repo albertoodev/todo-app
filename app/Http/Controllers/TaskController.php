e13c7c0b4dd9bd9cdfc6d5299e2c1ed806d0506f
albertoodev <ammaramoura12345@gmail.com>
2021-07-13 10:10:25 +0100

    2 sd commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,31 +12,43 @@ class TaskController extends Controller
 {
     public function fetchTasks(){
         $data = Task::where('stat','=',0)->get();
-        return  $data;
+        if(!$data){
+        return   response()->json(['data' => null , 'isEmpty' => true]);
+        }
+        else{
+            return   response()->json(['data' => $data , 'isEmpty' => false]);
+        }
     }
-
     public function fetchDoneTasks(){
         $data = Task::where('stat','=',1)->get();
-        return  $data;
+        if(!$data){
+            return   response()->json(['data' => null , 'isEmpty' => false]);
+            }
+            else{
+                return   response()->json(['data' => $data , 'isEmpty' => true]);
+            }
     }
-
     public function addTask(Request $request){
         $task = new Task;
         $task->name=$request->name;
-        return $task->save();         
+        $result=$task->save();
+        return   response()->json(['isAdded' => $result]);
     }
     public function delete($id){
         $task =Task::find($id);
-        return $task->delete();
+        $result =$task->delete();
+        return   response()->json(['isDeleted' => $result]);
     }
     public function update(Request $request,$id){
         $task =Task::find($id);
         $task->name=$request->name;
-        return  $task->save();
+        $result=$task->save();
+        return   response()->json(['isUpdated' => $result]);
     }
     public function setStat($id){
         $task =Task::find($id);
-        $task->stat=!$task->stat;
-        return $task->save();
+        $task->stat=true;
+        $result=$task->save();
+        return   response()->json(['isUpdated' => $result]);
     }
 }
